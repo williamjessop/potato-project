deed3353f6c05925f9cd1d9b86ce59cee9fb8514
wjessop <ycMGK4Kcp7io>
2020-12-14 20:22:43 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 function App() {
   return (
     <div className="App">
